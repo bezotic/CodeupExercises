f5c649903b87542d27b4bbcd90c351f502101c55
Richard <Richard@Richards-MacBook-Pro-2.local>
2016-03-01 21:01:24 -0600

    completed arithemtic. added function to warn users to add numbers only

--- arithmetic.php
+++ arithmetic.php
@@ -1,38 +1,59 @@
 <?php
 
-
-$a = 4;
-$b = 6;
-
-
-function add($a, $b)
-{
-    return $a + $b;
+function throwErrorMessage($a, $b, $divide = false) {
+	if (is_numeric($a)&& is_numeric($b)) {
+		return $a + $b;
+	} else {
+		echo "Error! {$a} and {$b} must be numbers\n";
+	}
+	if ($divide == false) {
+		echo "You cannot divide by zero" .PHP_EOL;
+	} else {
+		return true;
+	}
 }
 
-	echo add(2, 2) .PHP_EOL;
 
-function subtract($a, $b)
-{
-    return $a - $b;
+function add($a, $b){ 
+	     if (throwErrorMessage($a, $b)){
+	     	return $a + $b;
+	     }
+}
+
+	echo add(a, b) .PHP_EOL;
+
+function subtract($a, $b) {
+	if (throwErrorMessage($a, $b)) {
+		return $a - $b;
+	}
+    
+  
 }
 	echo subtract(5, 2) .PHP_EOL;
 
-function multiply($a, $b)
-{
-    return $a * $b;
+function multiply($a, $b) {
+ 	if (throwErrorMessage($a, $b)) {
+ 		return $a * $b;
+ 	}
+    
+   
 }
 	echo multiply(5, 5) .PHP_EOL;
 
-function divide($a, $b)
-{
-    return $a / $b;
+function divide($a, $b){
+	if (throwErrorMessage($a, $b)) {
+		 return $a / $b;
+	}
+   
+   
 }
-	echo divide(10,2) .PHP_EOL;
+	echo divide(10,0) .PHP_EOL;
 
-function modulus($a, $b)
-{
-	return $a % $b;
+function modulus($a, $b) {
+	if (throwErrorMessage($a, $b)) {
+		return $a % $b;
+	}
+	
 }
 	echo modulus(10,2) .PHP_EOL;
 	echo modulus(10,3) .PHP_EOL;
